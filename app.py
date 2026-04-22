85c31a7034d977531a0f7c3dc7bb47d2a3d493c0
saint <miessanhenri@gmail.com>
2026-04-22 15:01:51 +0000

    Remove app.run block and switch to Flask CLI

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-app.secret_key= os.getenv("SECRET_KEY")
+# app.secret_key= os.getenv("SECRET_KEY")
 # config the database first 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
