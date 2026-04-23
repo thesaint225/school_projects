0ad21deda0a93d54a6995dc76eb61e01fa7e8e03
saint <miessanhenri@gmail.com>
2026-04-23 04:26:01 +0000

    student.html added

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-# app.secret_key= os.getenv("SECRET_KEY")
+app.secret_key= os.getenv("SECRET_KEY")
 # config the database first 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -123,6 +123,11 @@ def teacher_dashboard():
 
     
     return f"welcome{session['user']}"
+
+
+@app.route('/student_dashboard')
+def student():
+    return render_template('student.html')
 
 
 @app.route('/logout')
